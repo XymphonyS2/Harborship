f3eb2ae73fcf852222cba89c4ce3f5497c0d8acd
XymphonyS2 <mybeautifulxymphony@gmail.com>
2024-12-13 10:27:20 +0700

    DATABASENIH

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'harborship';
+$dbname = 'harbroship';
 
 // Koneksi ke database
 $conn = new mysqli($host, $username, $password, $dbname);
