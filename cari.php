ff754f76c022bff14eaf69512a49d62c0e10ef7e
Bintang Prasetyo Kusumo Wicaksono <sutpidotdash@gmail.com>
2024-12-18 22:43:39 +0700

    cari1

--- cari.php
+++ cari.php
@@ -7,14 +7,30 @@
       <div class="row mb-3">
         <div class="col-md-6 mb-3">
           <label class="form-label">Pelabuhan Asal</label>
-          <select class="form-select">
-            <option>Pilih Asal</option>
+          <select name="rute_asal" class="form-select" required>
+            <option value="" hidden>Pilih Asal</option>
+            <?php
+            $query_rute_asal = query("SELECT * FROM rute");
+            while ($data_rute_asal = fetch($query_rute_asal)) {
+            ?>
+              <option value="<?= $data_rute_asal['pelabuhan_asal'] ?>"><?= $data_rute_asal['pelabuhan_asal'] ?></option>
+            <?php
+            }
+            ?>
           </select>
         </div>
         <div class="col-md-6 mb-3">
           <label class="form-label">Pelabuhan Tujuan</label>
-          <select class="form-select">
-            <option>Pilih Tujuan</option>
+          <select name="rute_tujuan" class="form-select" required>
+            <option value="" hidden>Pilih Tujuan</option>
+            <?php
+            $query_rute_tujuan = query("SELECT * FROM rute");
+            while ($data_rute_tujuan = fetch($query_rute_tujuan)) {
+            ?>
+              <option value="<?= $data_rute_tujuan['pelabuhan_tujuan'] ?>"><?= $data_rute_tujuan['pelabuhan_tujuan'] ?></option>
+            <?php
+            }
+            ?>
           </select>
         </div>
       </div>
@@ -23,14 +39,18 @@
       <div class="row mb-3">
         <div class="col-md-6 mb-3">
           <label class="form-label">Kelas Layanan</label>
-          <select class="form-select">
-            <option>Pilih Layanan</option>
+          <select class="form-select" name="kelas" required>
+            <option value="" hidden>Pilih Layanan</option>
+            <option value="express">Express</option>
+            <option value="reguler">Reguler</option>
           </select>
         </div>
         <div class="col-md-6 mb-3">
           <label class="form-label">Jenis Pengguna Jasa</label>
-          <select class="form-select">
-            <option>Pilih Jenis Pengguna</option>
+          <select class="form-select" name="jenis" required>
+            <option value="" hidden>Pilih Jenis Pengguna</option>
+            <option value="kendaraan">Kendaraan</option>
+            <option value="jalan_kaki">Jalan kaki</option>
           </select>
         </div>
       </div>
@@ -39,30 +59,30 @@
       <div class="row mb-3">
         <div class="col-md-6 mb-3">
           <label class="form-label">Jadwal Masuk Pelabuhan</label>
-          <input type="text" class="form-control" placeholder="dd/mm/yyyy">
+          <input type="date" class="form-control" name="tanggal">
         </div>
         <div class="col-md-6 mb-3">
           <label class="form-label">Pilih Jam</label>
-          <input type="text" class="form-control" placeholder="--:--">
+          <input name="jam" type="time" class="form-control">
         </div>
       </div>
 
       <div class="row mb-4">
         <div class="col-md-3 mb-3">
           <label class="form-label">Jumlah Lansia</label>
-          <input type="number" class="form-control" value="0" min="0">
+          <input type="number" name="lansia" class="form-control" value="0" min="0">
         </div>
         <div class="col-md-3 mb-3">
           <label class="form-label">Jumlah Dewasa</label>
-          <input type="number" class="form-control" value="0" min="0">
+          <input type="number" name="dewasa" class="form-control" value="0" min="0">
         </div>
         <div class="col-md-3 mb-3">
           <label class="form-label">Jumlah Anak</label>
-          <input type="number" class="form-control" value="0" min="0">
+          <input type="number" name="anak" class="form-control" value="0" min="0">
         </div>
         <div class="col-md-3 mb-3">
           <label class="form-label">Jumlah Bayi</label>
-          <input type="number" class="form-control" value="0" min="0">
+          <input type="number" name="bayi" class="form-control" value="0" min="0">
         </div>
       </div>
 
@@ -72,5 +92,13 @@
         </div>
       </div>
     </form>
+  </div>
+
+  <div class="search-container" style="padding-left: 20px; padding-right: 20px; padding-top: 10px; padding-bottom: 10px;">
+    <h4 class="mb-4">List Tiket</h4>
+
+    <!-- table -->
+     
+    
   </div>
 </main>
